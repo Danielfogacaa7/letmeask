6347a0e5c0816378e1fb28e8ba0cf8b8f62c4de7
Danielfogacaa7 <daniel@rocketit.com.br>
2021-07-14 22:16:37 -0300

    funcionalidade de like nas perguntas

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
 
 type FirebaseQuestions = Record<
 	string,
@@ -11,6 +12,7 @@ type FirebaseQuestions = Record<
 		content: string;
 		isAnswered: boolean;
 		isHighlighted: boolean;
+		likes: Record<string, { authorId: string }>;
 	}
 >;
 
@@ -23,15 +25,18 @@ type QuestionType = {
 	content: string;
 	isAnswered: boolean;
 	isHighlighted: boolean;
+	likeCount: number;
+	likeId: string | undefined;
 };
 
 export function useRoom(roomId: string) {
+	const { user } = useAuth();
 	const [questions, setQuestions] = useState<QuestionType[]>([]);
 	const [title, setTitle] = useState("");
 
 	useEffect(() => {
-		const rooRef = database.ref(`rooms/${roomId}`);
-		rooRef.on("value", (room) => {
+		const roomRef = database.ref(`rooms/${roomId}`);
+		roomRef.on("value", (room) => {
 			const databaseRoom = room.val();
 			const firebaseQuestions: FirebaseQuestions = databaseRoom.questions;
 			const parsedQuestions = Object.entries(firebaseQuestions).map(
@@ -42,13 +47,20 @@ export function useRoom(roomId: string) {
 						author: value.author,
 						isHighlighted: value.isHighlighted,
 						isAnswered: value.isAnswered,
+						likeCount: Object.values(value.likes ?? {}).length,
+						likeId: Object.entries(value.likes ?? {}).find(
+							([key, like]) => like.authorId === user?.id
+						)?.[0],
 					};
 				}
 			);
 			setTitle(databaseRoom.title);
 			setQuestions(parsedQuestions);
 		});
-	}, [roomId]);
+		return () => {
+			roomRef.off("value");
+		};
+	}, [roomId, user?.id]);
 
 	return { questions, title };
 }
